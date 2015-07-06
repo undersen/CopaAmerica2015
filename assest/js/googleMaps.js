72e2cc901c43cbb09f2d5b03dfa1cc106f1fee4e
undersen <under.sen@live.cl>
2015-07-06 01:17:31 -0300

    fixing some problems and bla bla bla

--- assest/js/googleMaps.js
+++ assest/js/googleMaps.js
@@ -5,11 +5,11 @@
             function init() {
 
             	var locations = [
-      ['<div> <h2>aa</h2><img class="thumb" src="assest/media/CHI.png"/> <img class="thumb" src="assest/media/ARG.png"/>', -33.4661619, -70.6106321],
-      ['Coogee Beach', -33.923036, 151.259052],
-      ['Cronulla Beach', -34.028249, 151.157507],
-      ['Manly Beach', -33.80010128657071, 151.28747820854187],
-      ['Maroubra Beach', -33.950198, 151.259302]
+      ['<div><h2>Estadio Nacional</h2><table class="table"><td><img class="thumb" src="assest/media/CHI.png"/><h3>2</h3></td> <td><img class="thumb" src="assest/media/ECU.png"/><h3>0</h3></td><tr><td><img class="thumb" src="assest/media/CHI.png"/><h3>3</h3></td><td><img class="thumb" src="assest/media/MEX.png"/><h3>3</h3></td><tr><td><img class="thumb" src="assest/media/CHI.png"/><h3>6</h3></td><td><img class="thumb" src="assest/media/BOL.png"/><h3>0</h3></td>', -33.4661619, -70.6106321],
+      ['<div> <h2>La portada</h2><img class="thumb" src="assest/media/CHI.png"/> <img class="thumb" src="assest/media/ARG.png"/>', -29.9113599, -71.2520006],
+      ['Cronulla Beach', -34.028249, -151.157507],
+      ['Manly Beach', -33.80010128657071, -151.28747820854187],
+      ['Maroubra Beach', -33.950198, -151.259302]
     ];
 
     
